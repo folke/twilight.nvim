f5718ddbd9bba3184493a4fc11cd38342966904f
Folke Lemaitre <folke.lemaitre@gmail.com>
2021-07-13 13:11:04 +0200

    fix: add setup to init

--- lua/twilight/init.lua
+++ lua/twilight/init.lua
@@ -1,15 +1,17 @@
 local view = require("twilight.view")
+local config = require("twilight.config")
 
 local M = {}
 
+M.setup = config.setup
 M.toggle = view.toggle
 M.disable = view.disable
 M.enable = view.enable
 
 function M.reset()
-	M.disable()
-	require("plenary.reload").reload_module("twilight")
-	require("twilight").enable()
+  M.disable()
+  require("plenary.reload").reload_module("twilight")
+  require("twilight").enable()
 end
 
 return M
