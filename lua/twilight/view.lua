0d1ba137ee5f1e42c0cd530b7e206a0d42549ee0
Mirko König <mirko.konig@here.com>
2022-02-10 18:23:35 +0100

    Highlight whole file if no matching root found.

--- lua/twilight/view.lua
+++ lua/twilight/view.lua
@@ -165,6 +165,9 @@ function M.get_expand_root(node, opts)
     end
     node = node:parent()
   end
+  if left_context > 0 then
+      return
+  end
   return root
 end
 
@@ -177,6 +180,11 @@ function M.get_context(buf, line)
       return from + 1, to + 2
     end
     local from, to = M.expand(buf, line, line, line)
+
+    if config.options.node_context > 0 then
+      local lcount = vim.api.nvim_buf_line_count(buf)
+      return 2, lcount  + 2
+    end
 
     while to - from < config.options.context do
       local pf, pt, pnode = M.expand(buf, from, to, from - 1)
